a063d03d63448d7a2ebbafb3b3b99efabed53633
TheoRice <theo.rice1996@gmail.com>
2018-03-06 11:11:57 -0500

    initial commit. All page templates with navigation bar included

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Website
+
+To run this locally, please type the command:
+python3 -m http.server
+
+This site will automatically start on localhost port 8000.
---
+++ js/main.js
@@ -0,0 +1,16 @@
+function toggleDropDown() {
+    document.getElementById('projectsDropdown').classList.toggle('show');
+}
+
+window.onclick = function (event) {
+    if (!(event.target.matches(".dropbtn") || event.target.matches(".dropbtnActive"))) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
+      }
+    }
+}
